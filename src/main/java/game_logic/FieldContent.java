52f33ee594fe5d64b3360c7f2309798a7ed0bc85
nick <konten@loewecke.de>
2021-03-29 21:28:57 +0200

    FieldContent linted

--- src/main/java/game_logic/FieldContent.java
+++ src/main/java/game_logic/FieldContent.java
@@ -1,12 +1,12 @@
 package game_logic;
 
 public enum FieldContent {
-    Empty("Empty",(byte)0),
-    Cathedral("Cathedral",(byte)1),
-    WhiteOccupied("Occupied by White",(byte)2),
-    BlackOccupied("Occupied by Black",(byte)3),
-    WhiteTerritory("White Territory",(byte)4),
-    BlackTerritory("Black Territory",(byte)5);
+    EMPTY("Empty",(byte)0),
+    CATHEDRAL("Cathedral",(byte)1),
+    WHITE_OCCUPIED("Occupied by White",(byte)2),
+    BLACK_OCCUPIED("Occupied by Black",(byte)3),
+    WHITE_TERRITORY("White Territory",(byte)4),
+    BLACK_TERRITORY("Black Territory",(byte)5);
 
     private final String name;
     private final byte value;
