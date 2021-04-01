246b9331beaabc6245b3e4e7fa8b5e6799658fd1
nick <konten@loewecke.de>
2021-04-01 16:27:47 +0200

    Make Building abstract and Adds getAllBuildingsForPlayer Method

--- src/main/java/game_logic/Building.java
+++ src/main/java/game_logic/Building.java
@@ -1,27 +1,36 @@
 package game_logic;
 
-import game_logic.buildings.Tavern;
+import game_logic.buildings.*;
 
-import java.awt.geom.Point2D;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Building {
+public abstract class Building {
     private final String name;
     private final Shape shape;
     private final Turnable turnable;
     private final Player player;
 
-    public Building(String name, Shape shape, Turnable turnable, Player player) {
+    protected Building(String name, Shape shape, Turnable turnable, Player player) {
         this.name = name;
         this.shape = shape;
         this.turnable = turnable;
         this.player = player;
     }
 
-    public List<Building> getDefaultBildings(Player player) {
-        List<Building> buildings = new ArrayList<>();
-        buildings.add(new Tavern(player));
+    public Map<Building, Integer> getAllBuildingsForPlayer(Player player) {
+        Map<Building, Integer> buildings = new HashMap<>();
+        buildings.put(new Tavern(player),2);
+        buildings.put(new Stable(player),2);
+        buildings.put(new Inn(player),2);
+
+        buildings.put(new Bridge(player),1);
+        buildings.put(new Manor(player),1);
+        buildings.put(new Square(player),1);
+        buildings.put(new Abbey(player),1);
+        buildings.put(new Infirmary(player),1);
+        buildings.put(new Castle(player),1);
+        buildings.put(new Tower(player),1);
 
         return buildings;
     }
