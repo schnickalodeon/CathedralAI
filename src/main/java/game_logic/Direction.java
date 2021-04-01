e6bb0b1d47f70905ec943f4ffbcb3795922b81b2
nick <konten@loewecke.de>
2021-04-01 16:37:49 +0200

    Adds Direction Enumeration

---
+++ src/main/java/game_logic/Direction.java
@@ -0,0 +1,15 @@
+package game_logic;
+
+public enum Direction {
+    D_0(0),
+    D_90(1),
+    D_180(2),
+    D_270(3);
+
+    private final int number;
+
+    Direction(int number) {
+        this.number = number;
+    }
+
+}
