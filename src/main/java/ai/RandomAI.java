cd3526e0acba2679f2fe6287e90c5ff598749bcc
nick <konten@loewecke.de>
2021-04-09 19:20:55 +0200

    Creates RandomAI

--- src/main/java/ai/RandomAI.java
+++ src/main/java/ai/RandomAI.java
@@ -1,13 +1,48 @@
 package ai;
 
 import game_logic.Board;
+import game_logic.Building;
+import game_logic.Direction;
 import game_logic.Move;
 import game_logic.buildings.Player;
 
+
+import java.awt.Point;
+import java.util.List;
+import java.util.Random;
+
 public class RandomAI implements AI{
+
+    private static final Random random = new Random();
+
 
     @Override
     public Move getMove(Board board, Player player) {
-        return null;
+
+        Point randomPoint = getRandomPoint();
+        Building randomBuilding = getRandomBuilding(player.getBuildings());
+        Direction randomDirection = getRandomDirection();
+
+        return new Move(randomPoint,randomBuilding,randomDirection,player);
     }
+
+    private static Direction getRandomDirection() {
+        int iRandom = random.nextInt(3);
+        return Direction.values()[iRandom];
+    }
+
+    private static Building getRandomBuilding(List<Building> buildings){
+        int buildingCount = buildings.size();
+        return buildings.get(random.nextInt(buildingCount));
+    }
+
+    private static Point getRandomPoint(){
+        int x = random.nextInt(9) + 1;
+        int y = random.nextInt(9) + 1;
+
+        return new Point(x,y);
+    }
+
+
+
 }
